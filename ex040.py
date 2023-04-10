3b146d264039c727e84751c26c86d8514df5f067
Maziko12 <128994920+Maziko12@users.noreply.github.com>
2023-04-10 19:59:03 -0300

    Outra mudança (Ex040)

--- ex040.py
+++ ex040.py
@@ -2,8 +2,8 @@ nota1 = float(input('Qual a primeira nota: '))
 nota2 = float(input('Qual a segunda nota: '))
 media = (nota1 + nota2) / 2
 if media <= 50.0:
-    print('Você foi reprovado!')
+    print(f'Com as notas sendo {nota1} e {nota2}. Você foi reprovado!')
 elif media >= 50.0 and media < 69.0:
-    print('Você está de RECUPERAÇÃO!')
+    print('Com as notas sendo {nota1} e {nota2}. Você está de RECUPERAÇÃO!')
 elif media >= 70.0:
-    print('Parabéns!! Você foi APROVADO!')
+    print(f'Parabéns!! Você foi APROVADO com uma média de {media}!')
